d3020f1eb32fb4970df91523866543b596364220
Nyurgun <burtsev.ns@gmail.com>
2022-03-08 23:27:20 +0900

    Initial Commit

---
+++ PhotoTapps/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PhotoTapps
+//
+//  Created by Нюргун on 08.03.2022.
+//  Copyright © 2022 Нюргун. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
